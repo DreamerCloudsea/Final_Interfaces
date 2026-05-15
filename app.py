c2ebeabb10a7da6fabdcd8414c0eac3c976acba6
DreamerCloudsea <karenh100305@gmail.com>
2026-05-14 21:08:37 -0500

    Uncomment model loading and cv2 import

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import numpy as np
 from PIL import Image;
-#from keras.models import load_model
-#import cv2
-#from keras.models import load_model
+import cv2
+from keras.models import load_model
 import paho.mqtt.client as mqtt
 import json
 import platform
